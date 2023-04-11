8f851efec0156ea94a33746e0dcabe882c79d842
Piero Viano <piero.viano@gmail.com>
2023-04-11 06:40:35 +0300

    ''

---
+++ Build.bat
@@ -0,0 +1,12 @@
+mklink /j Packages ..\WhenTheVersion\Packages
+del ..\WhenTheVersion\Packages\Net4x.Serilog.Sinks.Console.*
+rmdir /s /q %userprofile%\.nuget\Packages\Net4x.Serilog.Sinks.Console
+del "src\Serilog.Sinks.Console\bin\%Configuration%\Net4x.Serilog.Sinks.Console.*"
+MSBuild.exe serilog-sinks-console.sln -t:clean
+MSBuild.exe serilog-sinks-console.sln -t:restore -p:RestorePackagesConfig=true
+MSBuild.exe serilog-sinks-console.sln -m /property:Configuration=%Configuration% 
+copy "Serilog.Sinks.Console\bin\%Configuration%\Net4x.Serilog.Sinks.Console.*" Packages\
+git add -A
+git commit -a --allow-empty-message -m ''
+git push
+cd ..
